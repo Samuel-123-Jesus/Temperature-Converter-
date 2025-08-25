fc0fb4b9fc77d62c0c07a62367449c6093aa4986
Samuel-123-Jesus <okechukwuucheonye@gmail.com>
2025-08-25 19:07:17 +0100

    This is a temperature Converter
    Temperature Converter

---
+++ app.js
@@ -0,0 +1,54 @@
+function convertCelsiusToKelvin(temp)
+{let newTemp = temp + 273.15;
+
+return newTemp;
+}
+
+
+
+
+function convertCelsiusToFahrenheit(temp)
+
+{let newTemp = (temp * 9/5) + 32;
+
+return newTemp;
+}
+
+
+
+
+function convertKelvinToCelsius(temp)
+{let newTemp = temp - 273.15;
+
+return newTemp;
+}
+
+
+
+
+
+function convertKelvinToFahrenheit(temp)
+{let newTemp = (temp - 273.15) * 9/5 + 32;
+
+return newTemp;
+}
+
+
+
+
+
+function convertFahrenheitToCelsius(temp)
+{let newTemp = (temp - 32) * 5/9;
+
+return newTemp;
+}
+
+
+
+
+
+function convertFahrenheitToKelvin(temp)
+{let newTemp = (temp - 32) * 5/9 + 273.15;
+
+return newTemp;
+}
